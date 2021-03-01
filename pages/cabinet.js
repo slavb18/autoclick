5754d3f873e89821cc67c1c7cd180174b6a0476c
KsenyaMS <futya121992@gmail.com>
2021-03-01 21:00:43 +0400

    Создан прототип чата

--- pages/cabinet.js
+++ pages/cabinet.js
@@ -3,14 +3,13 @@ import { AutoForm } from 'uniforms-semantic';
 import { createSchemaBridge } from '../libs/uniforms';
 import PropTypes from 'prop-types'
 import React from 'react';
-import {
-  AutoFields, SubmitField,
-} from 'uniforms-semantic';
+import {AutoFields, SubmitField,} from 'uniforms-semantic';
 
-// import a1 from './dog/a1.png'
+
 import page1 from './dog/page-1.jpg'
 import page2 from './dog/page-2.jpg'
 import page3 from './dog/page-3.jpg'
+import a1 from './dog/a1.png'
 
 function ProductForm({ data, setData }) {
   const [open, setOpen] = React.useState(false);
@@ -26,75 +25,171 @@ function ProductForm({ data, setData }) {
     required: ['productAmount', 'downPayment', 'period', 'payment'],
   };
 
-  return <Segment>
-    <h1>Подобранный продукт</h1>
-    <table>
-      <tr>
-        <td>Срок</td>
-        <td>Ставка</td>
-        <td>Платеж</td>
-      </tr>
-      <tr>
-        <td>
-          {data.period} мес.
-        </td>
-        <td>
-          {data.rate} %
-        </td>
-        <td>
-          {data.payment} ₽
-        </td>
-      </tr>
-    </table>
-    <table>
-      <tr>
-        <td>Стоимость авто</td>
-        <td>Первоначальный взнос</td>
-      </tr>
-      <tr>
-        <td>
-          {data.productAmount} ₽
-        </td>
-        <td>
-          {data.downPayment} ₽
-        </td>
-      </tr>
-    </table>
+  return <div>
+    <Segment>
+      <h1>Подобранный продукт</h1>
+      <table>
+        <tr>
+          <td>Срок</td>
+          <td>Ставка</td>
+          <td>Платеж</td>
+        </tr>
+        <tr>
+          <td>
+            {data.period} мес.
+          </td>
+          <td>
+            {data.rate} %
+          </td>
+          <td>
+            {data.payment} ₽
+          </td>
+        </tr>
+      </table>
+      <table>
+        <tr>
+          <td>Стоимость авто</td>
+          <td>Первоначальный взнос</td>
+        </tr>
+        <tr>
+          <td>
+            {data.productAmount} ₽
+          </td>
+          <td>
+            {data.downPayment} ₽
+          </td>
+        </tr>
+      </table>
 
-    <Modal onClose={() => setOpen(false)}
-      onOpen={() => setOpen(true)}
-      open={open} trigger={<Button
-        content="Подобрать продукт"
-        labelPosition='right'
-        icon='calculator'
-        positive
-      />}>
-      <Modal.Header>
-        <em>Подобрать продукт</em>
-      </Modal.Header>
-      <Modal.Content>
-        <Modal.Description>
+      <Modal onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        open={open} trigger={<Button
+          content="Подобрать продукт"
+          labelPosition='right'
+          icon='calculator'
+          positive
+        />}>
+        <Modal.Header>
+          <em>Подобрать продукт</em>
+        </Modal.Header>
+        <Modal.Content>
+          <Modal.Description>
 
-        <AutoForm
-        schema={createSchemaBridge(schema)}
-        model={data}
-        onSubmit={onSubmit}
-        showInlineError={true}>
-            <AutoFields />
-            <SubmitField className="ui positive" value="Сохранить" />
-            <Button color='black' onClick={() => setOpen(false)}>
-              Закрыть
-            </Button>
+            <AutoForm
+              schema={createSchemaBridge(schema)}
+              model={data}
+              onSubmit={onSubmit}
+              showInlineError={true}>
+              <AutoFields />
+              <SubmitField className="ui positive" value="Сохранить" />
+              <Button color='black' onClick={() => setOpen(false)}>
+                Закрыть
+              </Button>
             </AutoForm>
-        </Modal.Description>
-      </Modal.Content>
-    </Modal>
-  </Segment>
+          </Modal.Description>
+        </Modal.Content>
+      </Modal>
+    </Segment>
+  </div>
 }
+
+
 
 ProductForm.propTypes = {
   data: PropTypes.object,
   setData: PropTypes.func,
+};
+
+function Chat({ data, setData }) {
+  const [open, setOpen] = React.useState(false);
+  const onSubmit = (formData) => { setOpen(false); setData(formData); }
+  const schema = {
+    title: 'Сообщения',
+    type: 'object',
+    properties: {
+      message: { type: 'string', title: 'Сообщения' },
+      },
+    required: ['message'],
+  };
+
+  const exit = [
+    { name: ' '},
+  ]
+  const interlocutor1 = [
+    { name: 'Собеседник1', src: a1 },
+  ]
+  const interlocutor2 = [
+    { name: 'Собеседник2', src: page1 },
+  ]
+
+  const panes = [
+    { menuItem: ' ', render: () => <Tab.Pane align="center">Выберите диалог</Tab.Pane> },
+    { menuItem: 'Собеседник1', render: () => <Tab.Pane><UploadMessages pages={interlocutor1} /></Tab.Pane> },
+    { menuItem: 'Собеседник2', render: () => <Tab.Pane><UploadMessages pages={interlocutor2} /></Tab.Pane> },
+  ]
+
+  return <div>
+    <Segment>
+      <Modal onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        open={open} trigger={<Button
+          content="Сообщения"  color='green' positive
+        />}>
+        <Modal.Header>
+          <em>Сообщения</em>
+        </Modal.Header>
+        <Modal.Content>
+          <Modal.Description>
+          <form>
+            <input type="text" id="filter" placeholder="Search for..." />
+          </form>
+          <Tab menu={{ fluid: true, vertical: true, tabular: true }} panes={panes} />
+          <Segment>
+          <AutoForm
+              schema={createSchemaBridge(schema)}
+              model={data}
+              onSubmit={onSubmit}
+              showInlineError={true}>
+              <AutoFields />
+              <SubmitField className="ui positive" value="Отправить" />
+              <Button color='black' onClick={() => setOpen(false)}>
+                Закрыть
+              </Button>
+            </AutoForm>
+            </Segment>
+          </Modal.Description>
+        </Modal.Content>
+      </Modal>
+    </Segment>
+  </div>
+}
+
+Chat.propTypes = {
+  data: PropTypes.object,
+  setData: PropTypes.func,
+};
+
+function UploadMessages({ pages }) {
+  return <div>
+    
+    <Grid columns={5} divided>
+      <Grid.Row>
+        {
+          pages.map((page, index) => <Grid.Column key={index}>
+            
+            {page.error && <span className="error">{page.error}</span>}
+            <br />
+            <img src={page.src} width="150" height="150" />
+          </Grid.Column>)
+        }
+
+      </Grid.Row>
+    </Grid>
+  </div>
+}
+
+UploadMessages.propTypes = {
+  pages: PropTypes.array,
 };
 
 function Upload({ pages }) {
@@ -190,6 +285,7 @@ export default function Cabinet(props) {
         </Grid.Column>
         <Grid.Column width="4">
           <ProductForm data={data} setData={setData} />
+          <Chat data={data} setData={setData} />
         </Grid.Column>
       </Grid.Row>
     </Grid>

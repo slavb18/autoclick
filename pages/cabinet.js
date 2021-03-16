1ceea0fc873ae03b2b2e87203426590517c9c33b
KsenyaMS <futya121992@gmail.com>
2021-03-16 23:36:11 +0400

    Исправления верстки

--- pages/cabinet.js
+++ pages/cabinet.js
@@ -1,4 +1,4 @@
-import { Modal, Button, Segment, Grid, Tab, Icon, Image} from 'semantic-ui-react'
+import { Modal, Button, Segment, Grid, Tab, Icon, Image, List} from 'semantic-ui-react'
 import { AutoForm } from 'uniforms-semantic';
 import { createSchemaBridge } from '../libs/uniforms';
 import PropTypes from 'prop-types'
@@ -291,134 +291,100 @@ function Error() {
 
 function ListForm() {
   return <div>
-  <Segment></Segment>
    <Grid divided='vertically'>
-      <Grid.Column width={4}>
+      <Grid.Column width={3}>
         <Segment>
           Загруженные документы
         </Segment>
       </Grid.Column>
-      <Grid.Column width={12}>
+      <Grid.Column width={13}>
         <Segment>
-          <Segment>
-            <Grid columns={3} divided>
-              <Grid.Column width={2}>
-                <img src={doc1} width="50" height="50" /> 
-              </Grid.Column>
-              <Grid.Column width={11}>
-                Договор№124432.png
-                <p>Добавлен 04.02.2021 в 11:46</p>
-              </Grid.Column>
-              <Grid.Column width={2}>
-                <Button color='green' content="Скачать" labelPosition='right'
-                icon='download'/>
-                <Button color='black' content="Удалить" labelPosition='right'
-                icon='trash alternate'/>
-              </Grid.Column>
-            </Grid>
-          </Segment>
-          <Segment>
-            <Grid columns={3} divided>
-              <Grid.Column width={2}>
-                <img src={doc2} width="55" height="50" /> 
-              </Grid.Column>
-              <Grid.Column width={11}>
-                Договор№54433.png
-                <p>Добавлен 02.03.2021 в 12:15</p>
-              </Grid.Column>
-              <Grid.Column width={2}>
-                <Button color='green' content="Скачать" labelPosition='right'
-                icon='download'/>
-                <Button color='black' content="Удалить" labelPosition='right'
-                icon='trash alternate'/>
-              </Grid.Column>
-            </Grid>
-          </Segment>
-          <Segment>
-            <Grid columns={3} divided>
-              <Grid.Column width={2}>
-                <img src={doc1} width="50" height="50" /> 
-              </Grid.Column>
-              <Grid.Column width={11}>
-                Договор.jpg
-                <p>Добавлен 01.03.2021 в 15:23</p>
-              </Grid.Column>
-              <Grid.Column width={2}>
-                <Button color='green' content="Скачать" labelPosition='right'
-                icon='download'/>
-                <Button color='black' content="Удалить" labelPosition='right'
-                icon='trash alternate'/>
-              </Grid.Column>
-            </Grid>
-          </Segment>
-          <Segment>
-            <Grid columns={3} divided>
-              <Grid.Column width={2}>
-                <img src={doc2} width="55" height="50" /> 
-              </Grid.Column>
-              <Grid.Column width={11}>
-                Согласие супруга.jpg
-                <p>Добавлен 28.02.2021 в 11:46</p>
-              </Grid.Column>
-              <Grid.Column width={2}>
-                <Button color='green' content="Скачать" labelPosition='right'
-                icon='download'/>
-                <Button color='black' content="Удалить" labelPosition='right'
-                icon='trash alternate'/>
-              </Grid.Column>
-            </Grid>
-          </Segment>
-          <Segment>
-            <Grid columns={3} divided>
-              <Grid.Column width={2}>
-                <img src={doc1} width="50" height="50" /> 
-              </Grid.Column>
-              <Grid.Column width={11}>
-                ИНН.jpg
-                <p>Добавлен 28.02.2021 в 11:42</p>
-              </Grid.Column>
-              <Grid.Column width={2}>
-                <Button color='green' content="Скачать" labelPosition='right'
-                icon='download'/>
-                <Button color='black' content="Удалить" labelPosition='right'
-                icon='trash alternate'/>
-              </Grid.Column>
-            </Grid>
-          </Segment>
-          <Segment>
-            <Grid columns={3} divided>
-              <Grid.Column width={2}>
-                <img src={doc1} width="50" height="50" /> 
-              </Grid.Column>
-              <Grid.Column width={11}>
-                Договор№98766.png
-                <p>Добавлен 26.02.2021 в 09:57</p>
-              </Grid.Column>
-              <Grid.Column width={2}>
-                <Button color='green' content="Скачать" labelPosition='right'
-                icon='download'/>
-                <Button color='black' content="Удалить" labelPosition='right'
-                icon='trash alternate'/>
-              </Grid.Column>
-            </Grid>
-          </Segment>
-          <Segment>
-            <Grid columns={3} divided>
-              <Grid.Column width={2}>
-                <img src={doc2} width="55" height="50" /> 
-              </Grid.Column>
-              <Grid.Column width={11}>
-                Согласие супруга.jpg
-                <p>Добавлен 24.02.2021 в 19:32</p>
-              </Grid.Column>
-              <Grid.Column width={2}>
-                <Button color='green' content="Скачать" labelPosition='right'
-                icon='download'/>
-                <Button color='black' content="Удалить" labelPosition='right'
-                icon='trash alternate'/>
-              </Grid.Column>
-            </Grid>
-          </Segment>
+          <List divided verticalAlign='middle'>
+            <List.Item>
+              <List.Content floated='right'>
+                <Button.Group vertical floated='right'>
+                  <Button color='green' content="Скачать" labelPosition='right'
+                  icon='download'/>
+                  <Button color='black' content="Удалить" labelPosition='right'
+                  icon='trash alternate'/>
+                </Button.Group>
+              </List.Content>
+              <Image doc src={doc1} width="50" height="50"/>
+              <List.Content>Договор№124432.png<p>Добавлен 04.02.2021 в 11:46</p></List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Content floated='right'>
+                <Button.Group vertical floated='right'>
+                  <Button color='green' content="Скачать" labelPosition='right'
+                  icon='download'/>
+                  <Button color='black' content="Удалить" labelPosition='right'
+                  icon='trash alternate'/>
+                </Button.Group>
+              </List.Content>
+              <Image doc src={doc2} width="55" height="50"/>
+              <List.Content>Договор№54433.png<p>Добавлен 02.03.2021 в 12:15</p></List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Content floated='right'>
+                <Button.Group vertical floated='right'>
+                  <Button color='green' content="Скачать" labelPosition='right'
+                  icon='download'/>
+                  <Button color='black' content="Удалить" labelPosition='right'
+                  icon='trash alternate'/>
+                </Button.Group>
+              </List.Content>
+              <Image doc src={doc1} width="50" height="50"/>
+              <List.Content>Договор.jpg<p>Добавлен 01.03.2021 в 15:23</p></List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Content floated='right'>
+                <Button.Group vertical floated='right'>
+                  <Button color='green' content="Скачать" labelPosition='right'
+                  icon='download'/>
+                  <Button color='black' content="Удалить" labelPosition='right'
+                  icon='trash alternate'/>
+                </Button.Group>
+              </List.Content>
+              <Image doc src={doc2} width="55" height="50"/>
+              <List.Content>Согласие супруга.jpg<p>Добавлен 28.02.2021 в 11:46</p></List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Content floated='right'>
+                <Button.Group vertical floated='right'>
+                  <Button color='green' content="Скачать" labelPosition='right'
+                  icon='download'/>
+                  <Button color='black' content="Удалить" labelPosition='right'
+                  icon='trash alternate'/>
+                </Button.Group>
+              </List.Content>
+              <Image doc src={doc1} width="50" height="50"/>
+              <List.Content>ИНН.jpg<p>Добавлен 28.02.2021 в 11:42</p></List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Content floated='right'>
+                <Button.Group vertical floated='right'>
+                  <Button color='green' content="Скачать" labelPosition='right'
+                  icon='download'/>
+                  <Button color='black' content="Удалить" labelPosition='right'
+                  icon='trash alternate'/>
+                </Button.Group>
+              </List.Content>
+              <Image doc src={doc1} width="50" height="50"/>
+              <List.Content>Договор№98766.png<p>Добавлен 26.02.2021 в 09:57</p></List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Content floated='right'>
+                <Button.Group vertical floated='right'>
+                  <Button color='green' content="Скачать" labelPosition='right'
+                  icon='download'/>
+                  <Button color='black' content="Удалить" labelPosition='right'
+                  icon='trash alternate'/>
+                </Button.Group>
+              </List.Content>
+              <Image doc src={doc2} width="55" height="50"/>
+              <List.Content>Согласие супруга.jpg<p>Добавлен 24.02.2021 в 19:32</p></List.Content>
+            </List.Item>
+          </List>
         </Segment>
       </Grid.Column>
    </Grid>
@@ -438,13 +404,17 @@ export default function Cabinet(props) {
         <Grid.Column width="12">
           <Error />
           <AutoDossier />
-          <ListForm/>
         </Grid.Column>
         <Grid.Column width="4">
           <Transition />
           <p></p>
           <ProductForm data={data} setData={setData} />
           <Chat data={data} setData={setData} />
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column width="15">
+          <ListForm/>
         </Grid.Column>
       </Grid.Row>
     </Grid>
